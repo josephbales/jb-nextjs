1debf3aed14a3d2e38a92d81942a8f233eae3ecd
Joseph Bales <joseph.bales@gmail.com>
2022-02-05 22:11:43 -0600

    typescriptifying

---
+++ pages/about.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+
+const About: NextPage = () => {
+    return(
+        <>
+            <p>
+                This is the personal blog of Joseph &quot;Joey&quot; Bales. All posts represent my personal views and opinions 
+                and may not represent the views and opinions of anyone else in particular. This site was resurrected 
+                on April 21, 2013. Who knows what happened to the content that came before...
+            </p>
+            <p>
+                As near as I can tell this site is <a href="https://en.wikipedia.org/wiki/General_Data_Protection_Regulation">GDPR</a> compliant,
+                so I see no reason to put any pop-ups or other crap on it. You are not tracked in any way when coming to 
+                this site.
+            </p>
+        </>
+    );
+}
+
+export default About;
---
+++ pages/contact.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+
+const Contact: NextPage = () => {
+    return(
+        <>
+            <p>
+                Constructive criticism, suggestions, and lucritive job offers are welcome.
+            </p>
+            <ul>
+                <li>Email: <a href="mailto:joey@josephbales.com">joey@josephbales.com</a></li>
+                <li>Github: <a href="https://github.com/josephbales">josephbales</a></li>
+            </ul>
+        </>
+    );
+}
+
+export default Contact;
